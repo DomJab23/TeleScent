525d353be4063ce2319e58fbe68ad26b3b87d998
monica1306 <monica.nolasco1306@gmail.com>
2025-12-11 12:28:14 +0100

    testcoverage

---
+++ test-coverage/main.js
@@ -0,0 +1,13 @@
+function add(a, b) {
+  return a + b;
+}
+
+function isEven(num) {
+  return num % 2 === 0;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+module.exports = { add, isEven, multiply };
